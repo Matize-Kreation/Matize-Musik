5931e0b9055793c5147a4f0770c9aee170a07154
Matize-Kreation <matize.kreation@gmail.com>
2025-11-06 15:19:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === "production";
 
-const repoName = "Matize-Musik"; // dein GitHub-Repo-Name
-
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    // alle Routen bekommen das Präfix
-    basePath: isProd ? `/${repoName}` : "",
-    // alle Assets (_next/static/...) auch
-    assetPrefix: isProd ? `/${repoName}/` : "",
-    trailingSlash: true,
-    staticPageGenerationTimeout: 180,
-    images: {
-        unoptimized: true,
-    },
+  output: "export",
+  images: {
+    // weil GitHub Pages kein Image Optimization-Server hat
+    unoptimized: true,
+  },
+  // wichtig für GitHub Pages unter /Matize-Musik
+  basePath: isProd ? "/Matize-Musik" : "",
+  assetPrefix: isProd ? "/Matize-Musik/" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
