cb7cad92d074279ab0fa537efa2f7e213d0697c4
mschlechtriem92-lab <m.schlechtriem92@gmail.com>
2025-11-06 14:16:59 +0100

    build: fresh export with assetPrefix

---
+++ docs/_next/static/chunks/app/musik/12-apokalypse/page-61120667be43ab42.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[909,56,407,105,588,526,420,533,955,674,442,822,687],{2887:function(n,e,u){Promise.resolve().then(u.t.bind(u,8173,23))}},function(n){n.O(0,[173,971,23,744],function(){return n(n.s=2887)}),_N_E=n.O()}]);
